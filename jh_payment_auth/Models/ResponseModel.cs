c20858822c98df8be7ed8b26d4f2720e67bc97a8
BottomHalf <istiyaq.mi9@gmail.com>
2025-09-08 20:07:15 +0530

    wrking code with health check enabled

---
+++ jh_payment_auth/Models/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace jh_payment_auth.Models
+{
+    public class ResponseModel
+    {
+        public string ResponseBody { set; get; }
+        public HttpStatusCode StatusCode { set; get; }
+        public string Message { set; get; }
+    }
+}
--- jh_payment_auth/Program.cs
+++ jh_payment_auth/Program.cs
@@ -3,6 +3,8 @@ using jh_payment_auth.Services;
 using jh_payment_auth.Validators;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +17,9 @@ builder.Services.AddDbContext<PaymentAuthDbContext>(options =>
 
 // Register the services for Dependency Injection.
 // The controller will receive the concrete implementations at runtime.
-builder.Services.AddSingleton<IUserService, UsersService>();
-builder.Services.AddSingleton<IUserRepository, UserRepository>();
-builder.Services.AddSingleton<IValidationService, ValidationService>();
+builder.Services.AddScoped<IUserService, UsersService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IValidationService, ValidationService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +28,20 @@ builder.Services.AddSwaggerGen(c =>
     var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
+});
+
+// Add API Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0); // Default: v1.0
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true; // Shows supported versions in headers
+
+    // Accept version from URL, header, or query string
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),   // ?api-version=1.0
+        new HeaderApiVersionReader("X-Version"),          // Header: X-Version: 1.0
+        new MediaTypeApiVersionReader("ver"));            // Header: Content-Type: application/json;ver=1.0
 });
 
 var app = builder.Build();
