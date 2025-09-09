e13ffa1d000bb974d37f79ffed3f110cfc132333
Jyoti Arya <Jyoti.Arya@celsiortech.com>
2025-09-09 12:05:31 +0530

    Jh_Auth_Login

---
+++ jh_payment_auth/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using jh_payment_auth.Models;
+using jh_payment_auth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jh_payment_auth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Models.LoginRequest request)
+        {
+            var result = _authService.Login(request);
+            if (result == null) return Unauthorized("Invalid username or password");
+            return Ok(result);
+        }
+
+        [HttpGet("secure-data")]
+        [Authorize]
+        public IActionResult GetSecureData()
+        {
+            var username = User.Identity?.Name;
+            return Ok(new { Message = $"Hello {username}, you are authenticated!" });
+        }
+    }
+}
